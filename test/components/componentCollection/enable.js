3a3c03a58a0a35b618f4013f76ac6966bef9b6d1
Romchik38 <pomahehko.c@gmail.com>
2026-03-15 13:58:38 +0200

    test: cc enable is ready

--- test/components/componentCollection/enable.js
+++ test/components/componentCollection/enable.js
@@ -9,10 +9,10 @@ import { default as Component } from '/js/components/component.js';
     var body = document.getElementsByTagName('body')[0];
     var div = document.createElement('input');
     div.setAttribute('type', 'text');
-    div.disabled = false;
+    div.disabled = true;
     var div2 = document.createElement('input');
     div2.setAttribute('type', 'text');
-    div2.disabled = false;
+    div2.disabled = true;
     body.appendChild(div);
     body.appendChild(div2);
     // create a component
@@ -21,14 +21,14 @@ import { default as Component } from '/js/components/component.js';
         new Component(div2)
     ];
     var ic = new ComponentCollection(components);
-    ic.disable();
+    ic.enable();
     // test value
     if (
-        div.disabled === true &&
-        div2.disabled === true
+        div.disabled === false &&
+        div2.disabled === false
     ) {
         console.log(testName + ' - passed');
     } else {
-        throw Error(testName + ' - expected true, found ' + div.disabled + ', ' + div2.disabled);
+        throw Error(testName + ' - expected false, found ' + div.disabled + ', ' + div2.disabled);
     }
 })();
