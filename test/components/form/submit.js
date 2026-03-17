aa9dbaa20fdb5efe264066583fd8d9100b4a712c
Romchik38 <pomahehko.c@gmail.com>
2026-03-17 13:26:57 +0200

    test: form submit is tested

---
+++ test/components/form/submit.js
@@ -0,0 +1,25 @@
+'use strict';
+
+import { default as Form } from '/js/components/form.js';
+
+(function(){
+    var testName = 'Test submit';
+    var value = 'Test 1';
+    var submitted = false;
+    // create tested element
+    var body = document.getElementsByTagName('body')[0];
+    var form = document.createElement('form');
+    form.submit = () => {
+        submitted = true;
+    };
+    body.appendChild(form);
+    // create a component
+    var f = new Form(form);
+    // test value
+    f.submit();
+    if (submitted === true) {
+        console.log(testName + ' - passed');
+    } else {
+        throw Error(testName + ' - expected true, found ' + submitted);
+    }
+})();
