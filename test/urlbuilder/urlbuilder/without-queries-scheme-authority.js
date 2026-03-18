bae841cc98ee9fa21f795f8e5324587b96c8cb1e
Romchik38 <pomahehko.c@gmail.com>
2026-03-18 12:44:40 +0200

    test(urlbuilder): relative url is ready

---
+++ test/urlbuilder/urlbuilder/without-queries-scheme-authority.js
@@ -0,0 +1,18 @@
+'use strict';
+
+import { default as urlbuilder } from '/js/urlbuilder/urlbuilder.js';
+import { default as target } from '/js/urlbuilder/target.js';
+
+(function(){
+    var testName = 'Test without queries scheme authority';
+    // create tested element
+    var parts = []
+    var u = urlbuilder(target);
+    // test
+    var url = u(parts);
+    if (url === '/') {
+        console.log(testName + ' - passed');
+    } else {
+        throw Error(testName + ' - expected /, found ' + url);
+    }
+})();
